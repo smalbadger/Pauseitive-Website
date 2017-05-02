8beceec98317b2d836fed0f45ca28f5f57c70273
Kinsleigh <kinsleighwong@email.arizona.edu>
2017-05-02 10:23:24 -0700

    Add files via upload

--- checkout.php
+++ checkout.php
@@ -7,30 +7,30 @@
 <fieldset class="purchaseContainer2">
             <h3>Address</h3>
             <label class = "heading" for = "street">Street:</label>
-            <input type="text" name="street"/>
+            <input type="text" name="street" required="required"/>
             <br/>
             <label class = "heading" for = "city">City:</label>
-            <input type="text" name="city"/>
+            <input type="text" name="city" required="required"/>
             <br/>
             <label class = "heading" for = "state">State:</label>
-            <input type="text" name="state"/>
+            <input type="text" name="state" required="required"/>
             <br/>
             <label class = "heading" for="zip">ZIP:</label>
-            <input type="text" name="zip"/>
+            <input type="text" name="zip" required="required"/>
             <br/>
             <label class = "heading" for="country">Country:</label>
-            <input type="text" name="country"/>
+            <input type="text" name="country" required="required"/>
             <br/>
 
             <h3>Billing Information</h3>
             <label class = "heading" for="cc">Credit Card Number:</label>
-            <input type="text" name="cc"/>
+            <input type="text" name="cc" required="required"/>
             <br/>
             <label class = "heading" for="csv">CSV Security Code:</label>
-			<input type="text" name="csv"/>
+			<input type="text" name="csv" required="required"/>
             <br/>
             <label class = "heading" for="exp">Expiration Date: </label>
-			<input type="text" name="exp"/>
+			<input type="text" name="exp" required="required"/>
             <br/>
             <input type="hidden" name="total" value=<?=$total?>>
             <input type="hidden" name="type" value="PURCHASE" required="required"/>
