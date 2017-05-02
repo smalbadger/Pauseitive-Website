8e6e9fee826977cb8af1847075820ad318c35297
Sam Badger <smalbadger@email.arizona.edu>
2017-05-02 15:02:00 -0700

    gave option to add administrator account
    In the comments in the register.php file, I put an option that allows you to add an admin account just by uncommenting the hidden input element.

--- register.php
+++ register.php
@@ -1,10 +1,10 @@
 <form action="validate.php" method="post">
   <fieldset class="purchaseContainer">
     <input type="hidden" name="register" value="True" />
-    <label class = "heading" for="firstname">First Name:</label> <input type="text" name="firstname" pattern="[A-Za-z]*" autofocus required="required"/> 
+    <label class = "heading" for="firstname">First Name:</label> <input type="text" name="firstname" pattern="[A-Za-z]*" autofocus required="required"/>
     <br/>
     <label class = "heading" for="lastname">Last Name:</label>
-    <input type="text" name="lastname" pattern="[A-Za-z]*" required="required"/> 
+    <input type="text" name="lastname" pattern="[A-Za-z]*" required="required"/>
     <br/>
     <label class = "heading" for="phone">Telephone number:</label>
     <input type="text" name="phone" pattern="\(\d{3}\) \d{3}-\d{4}" placeholder="(###) ###-####" required/> <br/>
@@ -19,9 +19,10 @@
     <input type="password" id="pass1" name="password" required="required" placeholder="password"/>
     <br/>
     <label class = "heading" for="userid">Confirm Password:</label>
-    <input type="password" id="pass2" name="confirm password" onkeyup="check_passwords();" required="required" placeholder="password"/> 
+    <input type="password" id="pass2" name="confirm password" onkeyup="check_passwords();" required="required" placeholder="password"/>
     <span class = "confirmMessage" id = "confirmMessage">  </span>
     <br/>
+    <!--<input type="hidden" name="type" value="Admin"/>-->  <!--Uncomment to add new administrator account.-->
     <div id="errors"></div>
     <div class="buttonsubmit"><input type="submit"/></div>
   </fieldset>
